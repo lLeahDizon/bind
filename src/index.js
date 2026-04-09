2fb22aeca45a2ef181ccfb17807fd0a770605a2c
lLeahDizon <leahyang@tungee.com>
2026-04-09 22:32:15 +0800

    feat: 实现 bind API

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var slice = Array.prototype.slice
+function bind(asThis) {
+  // this 就是函数
+  var args = slice.call(arguments, 1)
+  var fn = this
+  if (typeof fn !== 'function') {
+    throw new Error('bind 必须调用在函数身上')
+  }
+  return function() {
+    var args2 = slice.call(arguments, 0)
+    return fn.apply(asThis, args.concat(args2))
+  }
+}
+
+module.exports = bind;
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = bind
+}
---
+++ test/index.js
@@ -0,0 +1,25 @@
+import bind from '../src/index.js';
+
+Function.prototype.bind2 = bind
+console.assert(Function.prototype.bind2 !== undefined);
+
+const fn1 = function() {
+  return this;
+}
+
+const newFn1 = fn1.bind2({ name: "frank"});
+console.assert(newFn1().name === 'frank')
+
+const fn2 = function(p1, p2) {
+  return [this, p1, p2]
+}
+
+const newFn2 = fn2.bind2({name: 'frank'}, 124, 456)
+console.assert(newFn2()[0].name === 'frank', 'this')
+console.assert(newFn2()[1] === 124, 'p1')
+console.assert(newFn2()[2] === 456, 'p2')
+
+const anotherFn2 = fn2.bind2({name: 'frank'}, 123)
+console.assert(anotherFn2(245)[0].name === 'frank', 'this')
+console.assert(anotherFn2(245)[1] === 123, 'p1')
+console.assert(anotherFn2(245)[2] === 245, 'p22')
